2c58f3969ff978c22b72b89003d5c3d4007c536e
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-05-01 23:15:42 -0400

    page classs

--- src/main/java/pages/TestTablePage.java
+++ src/main/java/pages/TestTablePage.java
@@ -19,6 +19,12 @@ public class TestTablePage {
 	private By dropDown = By.xpath("//legend[text()='Min enrollments']/following::div[@id='enrollDropdown']");
 	private By enrollOps = By.xpath("//table//tr[not(contains(@style, 'display: none'))]/td[5]");
 	private By anyOptions = By.xpath("//ul[@class='dropdown-menu']//li[text()='10,000+']");
+	
+	private By pythonOp = By.xpath("//legend[text()='Language']//following-sibling::label[3]");
+	private By anyOption = By.xpath("//div[@class=\"dropdown-button\"]//following-sibling::ul//li[3]");
+	private By pythonTableEle = By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[3]");
+	private By beginnerTableEle = By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[4]");
+	private By enrollTableEle = By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[5]");
 
 	public void selectLanguageJava() {
 		UiActions.click(javaOp, "Java option selected");
@@ -57,8 +63,22 @@ public class TestTablePage {
 		return UiActions.getElements(enrollOps,"enrollments range");
 	}
 	
-	
-	
+	public void selectPythonLanguage()
+	{
+		UiActions.click(pythonOp, "Python language selected");
+	}
+	public String getPythonElementText()
+	{
+		return UiActions.getText(pythonTableEle, "Python Element");
+	}
+	public String getBeginnerElememtText()
+	{
+		return UiActions.getText(beginnerTableEle, "Beginner Element");
+	}
+	public String getEnrollElementText()
+	{
+		return UiActions.getText(enrollTableEle, "Enrollment Text");
+	}
 	
 	
 	
